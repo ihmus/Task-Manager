1e3b4f520f5cd5caf13e19ed723c6e3e7a91dd89
İhMus <112729973+ihmus@users.noreply.github.com>
2025-10-21 00:04:27 +0300

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,4 @@
-
-
-
-
-"""from flask import Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -44,18 +40,3 @@ def create_database(app):
         with app.app_context():
             db.create_all()
             print('Created Database')
-"""
-from flask import Flask
-
-
-def create_app():
-    app= Flask(__name__)
-    app.config["SECRET_KEY"]="BİSMİLLAHİRRAHMANİRRAHİM"
-    
-
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views,url_prefix="/")
-    app.register_blueprint(auth,url_prefix='/')
-    return app
