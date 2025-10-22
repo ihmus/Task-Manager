24edff87b49348bc56da8cdafa960ab638068228
İhMus <112729973+ihmus@users.noreply.github.com>
2025-10-22 13:01:59 +0300

    renklendirme vs yapıldı

--- app/models.py
+++ app/models.py
@@ -8,11 +8,17 @@ class Note(db.Model):
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+####################    ^      #######################
+#                       |      #######################
+#                       |      #######################
+#                       |      #######################
+#                       |      #######################
+#    userin id bilgisinide bu tabloya veriyoruz      #
+######################################################
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    notes = db.relationship('Note') # Burada ilişki verdik ileride user üzerinden ulaşabilmek için
